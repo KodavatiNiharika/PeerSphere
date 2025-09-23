7cd7918cdb59499bd5a46bd686f670ca120a01c1
KodavatiNiharika <kodavatiniharika@gmail.com>
2025-09-23 15:24:41 +0530

    Update Register.js

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -37,7 +37,7 @@ const Register = () => {
     setSuccess("");
 
     try {
-      const response = await axios.post("http://localhost:3001/register", {
+      const response = await axios.post(" https://peersphere-3.onrender.com/register", {
         username,
         email,
         password,
